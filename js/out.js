23c00ca3ba72732e49bf1b19e660727d836b691e
Jan Troler <jan.troler@galtys.com>
2021-05-06 19:07:42 +0200

    step

--- js/out.js
+++ js/out.js
@@ -1,7 +1,7 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var W3CWebSocket = require('websocket').w3cwebsocket;
 
-var client = new W3CWebSocket('ws://192.168.1.149:8080/', 'echo-protocol');
+var client = new W3CWebSocket('ws://192.168.1.149:8080/muf', 'echo-protocol');
 
 client.onerror = function() {
     console.log('Connection Error');
